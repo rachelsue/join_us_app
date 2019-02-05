4fa631c0f01df03e0eece5e477e74fda3eab4941
rachelsue <wells.rachelsue@gmail.com>
2019-02-05 12:27:16 -0600

    connect mysql

---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (email) VALUES ('Katie34@yahoo.com'), ('Tunde@gmail.com');
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var faker = require('faker');
 var mysql = require('mysql');
-var express = require("express");
+// var express = require("express");
 var path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+// var app = express();
+// var PORT = process.env.PORT || 8080;
 
 // var db = require("./db");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 var connection = mysql.createConnection ({
     host: 'localhost',
@@ -21,24 +21,13 @@ var connection = mysql.createConnection ({
     database: 'join_us'
 }); 
 
-var q = 'SELECT CURTIME() AS time, CURDATE() AS date, NOW() AS now'
+var q = 'SELECT COUNT(*) AS total FROM users';
+
 connection.query(q, function(err, results, fields) {
   if (err) throw err;
-  console.log(results[0].time);
-  console.log(results[0].date);
-  console.log(results[0].now);
-})
+  console.log(results[0].total);
+});
 
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("connected on " + connection.threadId);
-//     connection.end();
-//   });
-
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
- 
-
+connection.end();
 
   
